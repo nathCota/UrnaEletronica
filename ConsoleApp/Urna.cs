88cc71b82b13d45ff7f5dce452fc748aad6f148b
jeffpaulinelli <jeffalvesg@hotmail.com>
2022-12-04 23:47:46 -0300

    -commit correção para adm funcionar

--- ConsoleApp/Urna.cs
+++ ConsoleApp/Urna.cs
@@ -20,6 +20,7 @@ namespace ConsoleApp
         public static ArrayList Politicos { get; set; } = new ArrayList();
         internal static void MenuIniciaL()
         {
+
             Console.Clear();
             Console.WriteLine("VOTAÇÃO\n------------------------------");
             GestaoEleicao.ListarEleicoes();
@@ -36,16 +37,40 @@ namespace ConsoleApp
                 valida = Servico.VerificaExisteId(id);
             }
             int escolhaModulo;
-            // if(escolhaModulo==)
+            ModuloExecutivo();
 
         }
-        public static Candidato ModuloLegislativo(){
+        public static void ModuloExecutivo()
+        {
+            PesquisaPresidente();
+        }
+        public static string PesquisaPresidente(){
             try
             {
                 var candidatos = JsonParaObjeto<Candidatos>.Recuperar(new Candidatos());
                 var partidos = JsonParaObjeto<Partidos>.Recuperar(new Partidos());
-
                 if (candidatos == null || candidatos.ListaCandidatos.Count == 0 || partidos == null || partidos.ListaPartidos.Count == 0)
+                {
+                    return default;
+                }
+
+                ConsoleTable tabela = new ConsoleTable("Id", "Nome", "Partido", "Cargo do Candidato");
+                foreach (Candidato candidato in candidatos.ListaCandidatos)
+                {
+                    if (candidato.CardgoCandidato==0) {
+                        var partido = partidos?.ListaPartidos.Find(x => x.Id == candidato.Partido);
+                        tabela.AddRow(candidato.Id, candidato.Nome, $"{partido?.Nome} - {partido?.Sigla}", candidato.CardgoCandidato);
+                    }
+                }
+
+                return tabela.ToString();
+            }
+            catch
+            {
+                return default;
+            }
+            /*
+            if (candidatos == null || candidatos.ListaCandidatos.Count == 0 || partidos == null || partidos.ListaPartidos.Count == 0)
                 {
                     return default;
                 }
@@ -62,18 +87,14 @@ namespace ConsoleApp
             catch
             {
                 return default;
-            }
+            }*/
+
         }
             
             //testar todos os tipo de candidato, retornando a listagem e pedindo para escolher, no final de cada tipo computar o voto 
-            ComputaVoto();
+            // ComputaVoto();
         
-        public static void ModuloExecutivo()
-        {
 
-            //testar todos os tipo de candidato, retornando a listagem e pedindo para escolher, no final de cada tipo computar o voto 
-            ComputaVoto();
-        }
         internal static void ComputaVoto()
         {
 
